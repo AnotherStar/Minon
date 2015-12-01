a0c1560d447e72875ee776e2cda32e12cc032f8c
AliumAstrum <design@spartak.ru>
2015-12-01 16:40:14 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,36 +1,8 @@
-alert('before ready');
+document.addEventListener("deviceready", onDeviceReady(), true);
 
-$(document).ready(function() {
-	alert('ready');
-	document.addEventListener('deviceready', init_app, false);
-});
-
-function init_app(){
-	alert('init_app');
-	$('#console').text('Ready!');
-	$('#console').append(getAjax());
-	$('#console').append(getAjax());
-}
-
-function getAjax(){
-	alert('getAjax');
-	var result;
-	$.ajax({
-		url: 'http://ajax.minon.ru/minon.php',
-		type: 'POST',
-		dataType: 'html',
-		async: false,
-		data: {action: 'helloWorld'},
-	})
-	.done(function(data) {
-		$('#console').append(' done ');
-		result = data;
-	})
-	.fail(function(data) {
-		console.log(data);
-	})
-	.always(function() {
-		$('#console').append(' always ');
+function onDeviceReady(){
+	alert('onDeviceReady');
+	$(document).ready(function() {
+		alert('ready');
 	});
-	return result;
 }
