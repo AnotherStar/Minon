2d72e65edc15cc3f433a5d593314c402a679fea0
AliumAstrum <design@spartak.ru>
2015-12-01 16:52:30 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-document.addEventListener("deviceready", onDeviceReady(), true);
+alert('first');
+
+
+$(document).ready(function() {
+	alert('ready');
+	document.addEventListener("deviceready", onDeviceReady(), true);
+	$('#console').text(Date.now);
+});
+
 
 function onDeviceReady(){
 	alert('onDeviceReady');
-	$(document).ready(function() {
-		alert('ready');
-		$('#console').text(Date.now);
-	});
 }
 
