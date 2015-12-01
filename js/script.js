e3b434a5f641772ebdbf66b5fc95626aafe8e123
AliumAstrum <design@spartak.ru>
2015-12-01 14:07:58 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-	alert('Ready!');
+	$('#console').text('Ready!');
+	$('#console').append(getAjax());
 });
+
+function getAjax(){
+	var result;
+	$.ajax({
+		url: 'http://ajax.minon.ru/minon.php',
+		type: 'POST',
+		dataType: 'html',
+		async: false,
+		data: {action: 'helloWorld'},
+	})
+	.done(function(data) {
+		result = data;
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	return result;
+}
