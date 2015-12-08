50e2545fcff2183490d0871211f680b51c6d9ecd
AliumAstrum <design@spartak.ru>
2015-12-08 16:10:55 +0300

    1

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 
 function onDeviceReady(){
 ajax();
+initPushwoosh();
 }
 
 function ajax(data){
@@ -30,6 +31,37 @@ function ajax(data){
 	});
 	$('#console').text(result);
 	return result;
+}
+
+function initPushwoosh()
+{
+    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+ 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_NUMBER", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "862592340817", pw_appid : "FCBA5-CAA7F" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
 }
 
 
