b2929600833436776651d6dad8b264a46cb99a78
AliumAstrum <design@spartak.ru>
2015-12-15 16:10:07 +0300

    1

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ function onDeviceReady(){
 	ajax();
 }
 
-function ajax(data){
+function ajax(){
 	var result;
 	$.ajax({
 		url: 'http://ajax.minon.ru/minon.php',
 		type: 'POST',
 		dataType: 'html',
-		data: {data},
+		data: {},
 		async: false,
 	})
 	.done(function(data) {
@@ -33,39 +33,39 @@ function ajax(data){
 }
 
 // Register for any urban airship events
-document.addEventListener("urbanairship.registration", function (event) {
-    if (event.error) {
-        console.log('there was an error registering for push notifications');
-    } else {
-        console.log("Registered with ID: " + event.pushID);
-    } 
-}, false)
+// document.addEventListener("urbanairship.registration", function (event) {
+//     if (event.error) {
+//         console.log('there was an error registering for push notifications');
+//     } else {
+//         console.log("Registered with ID: " + event.pushID);
+//     } 
+// }, false)
 
-document.addEventListener("urbanairship.push", function (event) {
-    console.log("Incoming push: " + event.message)
-}, false)
+// document.addEventListener("urbanairship.push", function (event) {
+//     console.log("Incoming push: " + event.message)
+// }, false)
 
-// Set tags on a device, that you can push to
-// https://docs.urbanairship.com/display/DOCS/Server%3A+Tag+API
-PushNotification.setTags(["loves_cats", "shops_for_games"], function () {
-    PushNotification.getTags(function (obj) {
-        obj.tags.forEach(function (tag) {
-            console.log("Tag: " + tag);
-        });
-    });
-});
+// // Set tags on a device, that you can push to
+// // https://docs.urbanairship.com/display/DOCS/Server%3A+Tag+API
+// PushNotification.setTags(["loves_cats", "shops_for_games"], function () {
+//     PushNotification.getTags(function (obj) {
+//         obj.tags.forEach(function (tag) {
+//             console.log("Tag: " + tag);
+//         });
+//     });
+// });
 
-// Set an alias, this lets you tie a device to a user in your system
-// https://docs.urbanairship.com/display/DOCS/Server%3A+iOS+Push+API#ServeriOSPushAPI-Alias
-PushNotification.setAlias("awesomeuser22", function () {
-    PushNotification.getAlias(function (alias) {
-        console.log("The user formerly known as " + alias)
-    });
-});
+// // Set an alias, this lets you tie a device to a user in your system
+// // https://docs.urbanairship.com/display/DOCS/Server%3A+iOS+Push+API#ServeriOSPushAPI-Alias
+// PushNotification.setAlias("awesomeuser22", function () {
+//     PushNotification.getAlias(function (alias) {
+//         console.log("The user formerly known as " + alias)
+//     });
+// });
 
-// Check if push is enabled
-PushNotification.isPushEnabled(function (enabled) {
-    if (enabled) {
-        console.log("Push is enabled! Fire away!");
-    }
-})
+// // Check if push is enabled
+// PushNotification.isPushEnabled(function (enabled) {
+//     if (enabled) {
+//         console.log("Push is enabled! Fire away!");
+//     }
+// })
